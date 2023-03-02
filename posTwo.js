db88d4b6250e14ee385dae6a1905ea2df4dae417
Brandon Soucie <brandonsoucie@rocketmortgage.com>
2023-03-01 22:12:41 -0500

    inital inventory array

---
+++ posTwo.js
@@ -0,0 +1,83 @@
+const inventory = [
+    {
+        category: 'Cookie',
+        name:'Chocolate Chip',
+        price: 1.00,
+        description: 'Classic Milk Chocolate Chip Cookie',  /// all descriptions are placeholders for now
+    },
+    {
+        category: 'Cookie',
+        name:'Peanut Butter',
+        price: 1.00,
+        description: 'Pure Peanut Butter Heaven'
+    },
+    {
+        category: 'Cookie',
+        name: 'Double Chocolate',
+        price: 1.25,
+        description: 'Double the Decadance, Double the Chocolate'
+    },
+    {
+        category: 'Cookie',
+        name: 'White Chocolate',
+        price: 1.75,
+        description: 'White Chocolate Wonderfulness'
+
+    },
+    {
+        category: 'Donuts',
+        name: 'Chocolate Glazed',
+        price: 1.00,
+        description: 'yup',
+    },
+    {
+        category: 'Donuts',
+        name: 'Powdered Sugar',
+        price: 1.00,
+        description: 'yum',
+
+    },
+    {
+        category: 'Donuts',
+        name: 'Apple Fritter',
+        price: 1.50,
+        description: 'fritter-tastic',
+        
+    },
+    {
+        category: 'Donuts',
+        name: 'Cinnamon Roll',
+        price: 1.75,
+        description: 'roll it up',
+
+    },
+    {
+        category: 'Pies',
+        name: 'Apple',
+        price: 10.00,
+        description: "'Merica",
+
+    },
+    {
+        category: 'Pies',
+        name: 'Cherry',
+        price: 10.00,
+        description: 'very cherry',
+
+    },
+
+    {
+        category: 'Pies',
+        name: 'Key Lime',
+        price: 12.00,
+        description: 'key to happiness',
+    },
+    {
+        category: 'Pies',
+        name: 'Pecan',
+        price: 12.00,
+        description: 'you can pecan',
+
+    }
+]
+

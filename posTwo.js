3457f20acb81629d681e137e5a813d228745aa79
Brandon Soucie <brandonsoucie@rocketmortgage.com>
2023-03-02 20:06:04 -0500

    once again again

--- posTwo.js
+++ posTwo.js
@@ -1,27 +1,38 @@
+(function () {
+
 const inventory = [
     {
         category: 'Cookie',
         name:'Chocolate Chip',
         price: 1.00,
         description: 'Classic Milk Chocolate Chip Cookie',  /// all descriptions are placeholders for now
+        id: 
+        image: 
+
     },
     {
         category: 'Cookie',
         name:'Peanut Butter',
         price: 1.00,
-        description: 'Pure Peanut Butter Heaven'
+        description: 'Pure Peanut Butter Heaven',
+        id:
+        image:
     },
     {
         category: 'Cookie',
         name: 'Double Chocolate',
         price: 1.25,
-        description: 'Double the Decadance, Double the Chocolate'
+        description: 'Double the Decadance, Double the Chocolate',
+        id:
+        image:
     },
     {
         category: 'Cookie',
         name: 'White Chocolate',
         price: 1.75,
-        description: 'White Chocolate Wonderfulness'
+        description: 'White Chocolate Wonderfulness',
+        id:
+        image:     
 
     },
     {
@@ -29,12 +40,16 @@ const inventory = [
         name: 'Chocolate Glazed',
         price: 1.00,
         description: 'yup',
+        id:
+        image:
     },
     {
         category: 'Donuts',
         name: 'Powdered Sugar',
         price: 1.00,
         description: 'yum',
+        id:
+        image:
 
     },
     {
@@ -42,6 +57,8 @@ const inventory = [
         name: 'Apple Fritter',
         price: 1.50,
         description: 'fritter-tastic',
+        id:
+        image:
         
     },
     {
@@ -49,6 +66,8 @@ const inventory = [
         name: 'Cinnamon Roll',
         price: 1.75,
         description: 'roll it up',
+        id:
+        image:
 
     },
     {
@@ -56,6 +75,8 @@ const inventory = [
         name: 'Apple',
         price: 10.00,
         description: "'Merica",
+        id: 0
+
 
     },
     {
@@ -63,6 +84,8 @@ const inventory = [
         name: 'Cherry',
         price: 10.00,
         description: 'very cherry',
+        id:
+        image:
 
     },
 
@@ -71,13 +94,48 @@ const inventory = [
         name: 'Key Lime',
         price: 12.00,
         description: 'key to happiness',
+        id:
+        image:
     },
     {
         category: 'Pies',
         name: 'Pecan',
         price: 12.00,
         description: 'you can pecan',
+        id:
+        image:
 
     }
 ]
 
+
+
+
+
+
+
+
+
+function updateTotal () {
+    let total = 0;
+    cart.forEach((item) => (total =+item.price));
+    document.querySelector(".total").innerText = `$${total}`;
+
+        
+    }
+}
+    
+   
+        
+    
+
+
+
+
+
+
+
+
+
+
+})();

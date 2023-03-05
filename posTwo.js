1447d2461978c665b486d9b3a8efef345eb9087a
Kate Grutter <kategrutter@gmail.com>
2023-03-05 11:32:28 -0500

    combining pos and backerycheckout html, css and js

--- posTwo.js
+++ posTwo.js
@@ -164,6 +164,43 @@ function updateTotal() {
 
 }
 
+//to open checkout
+
+  const checkOutButton = document.getElementById('open-checkout');
+  checkOutButton.addEventListener('click', event => {
+    const checkoutPopUp = document.querySelector('.checkout-popup');
+    checkoutPopUp.style.display = 'block';
+
+  })
+
+  //BAKERYCHECKOUTJS
+
+  const cashButton = document.getElementById('cash');
+const cardButton = document.getElementById('card');
+const purchaseButton = document.getElementById('subBut');
+
+cashButton.addEventListener('click', () => {
+    const cashForm = document.getElementById('ifCash');
+    const cardForm = document.getElementById('ifCard');
+    cashForm.style.display = 'block';
+    cardForm.style.display = 'none'
+});
+
+cardButton.addEventListener('click', () => {
+    const cardForm = document.getElementById('ifCard');
+    const cashForm = document.getElementById('ifCash')
+    cardForm.style.display = 'block';
+    cashForm.style.display = 'none'
+
+});
+purchaseButton.addEventListener('click', () => {
+    const receiptForm = document.getElementById('rec');
+
+    receiptForm.style.display = 'block';
+
+
+})
+
 
 
 })();

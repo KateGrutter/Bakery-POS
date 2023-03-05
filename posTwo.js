643e632cec914f4dc8a0be969726f8792224c148
Bailey Cytacki-Lewis <baileycytackilewis@gmail.com>
2023-03-05 18:05:37 -0500

    added some of the code from my fork

--- posTwo.js
+++ posTwo.js
@@ -7,6 +7,7 @@ const inventory = [
         description: 'Classic Milk Chocolate Chip Cookie',  /// all descriptions are placeholders for now
         id: 'choc-chip',
         image: "assets/food-photographer-jennifer-pallian-OfdDiqx8Cz8-unsplash.jpg",
+        quantity: 0
         
     },
     {
@@ -16,6 +17,7 @@ const inventory = [
         description: 'Pure Peanut Butter Heaven',
         id: 'pb',
         image: "assets/priscilla-du-preez--Y-qg-ncD34-unsplash.jpg",
+        quantity: 0
     },
     {
         category: 'Cookie',
@@ -23,7 +25,8 @@ const inventory = [
         price: 1.25,
         description: 'Double the Decadance, Double the Chocolate',
         id: 'dbl-choc',
-        image: "assets/julissa-capdevilla-HHee8b4ZAlU-unsplash.jpg"
+        image: "assets/julissa-capdevilla-HHee8b4ZAlU-unsplash.jpg",
+        quantity: 0
     },
     {
         category: 'Cookie',
@@ -31,7 +34,8 @@ const inventory = [
         price: 1.75,
         description: 'White Chocolate Wonderfulness',
         id: 'w-choc',
-        image: "assets/morgane-perraud-XhrW0XlRy8c-unsplash.jpg"
+        image: "assets/morgane-perraud-XhrW0XlRy8c-unsplash.jpg",
+        quantity: 0
 
     },
     {
@@ -40,7 +44,8 @@ const inventory = [
         price: 1.00,
         description: 'yup',
         id: 'choc-glazed',
-        image: 'https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80'
+        image: 'https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1664&q=80',
+        quantity: 0
     },
     {
         category: 'Donuts',
@@ -48,7 +53,8 @@ const inventory = [
         price: 1.00,
         description: 'yum',
         id: 'pow-sug',
-        image: "assets/no-revisions-vUSfCfuq-2Y-unsplash.jpg"
+        image: "assets/no-revisions-vUSfCfuq-2Y-unsplash.jpg",
+        quantity: 0
 
     },
     {
@@ -57,7 +63,8 @@ const inventory = [
         price: 1.50,
         description: 'fritter-tastic',
         id: 'fritter',
-        image: "assets/katherine-sousa-k2UM3jOgmGI-unsplash.jpg"
+        image: "assets/katherine-sousa-k2UM3jOgmGI-unsplash.jpg",
+        quantity: 0
         
     },
     {
@@ -66,7 +73,8 @@ const inventory = [
         price: 1.75,
         description: 'roll it up',
         id: 'roll',
-        image: "assets/andy-hay-1uaIZcCJ-H0-unsplash.jpg"
+        image: "assets/andy-hay-1uaIZcCJ-H0-unsplash.jpg",
+        quantity: 0
     },
     {
         category: 'Pies',
@@ -74,7 +82,8 @@ const inventory = [
         price: 10.00,
         description: "'Merica",
         id: 'apple',
-        image: "assets/kavya-p-k-zQ4jrYelvLs-unsplash.jpg"
+        image: "assets/kavya-p-k-zQ4jrYelvLs-unsplash.jpg",
+        quantity: 0
 
     },
     {
@@ -83,7 +92,8 @@ const inventory = [
         price: 10.00,
         description: 'very cherry',
         id: 'cherry',
-        image: "assets/marie-g-geri6ml5e2I-unsplash.jpg"
+        image: "assets/marie-g-geri6ml5e2I-unsplash.jpg",
+        quantity: 0
 
     },
 
@@ -93,7 +103,8 @@ const inventory = [
         price: 12.00,
         description: 'key to happiness',
         id: 'key',
-        image: "assets/kaouther-djouada-j8ufR4H4PaE-unsplash.jpg"
+        image: "assets/kaouther-djouada-j8ufR4H4PaE-unsplash.jpg",
+        quantity: 0
     },
     {
         category: 'Pies',
@@ -101,7 +112,8 @@ const inventory = [
         price: 12.00,
         description: 'you can pecan',
         id: 'pecan',
-        image: "assets/keighla-exum-wnn9GuvXIZ4-unsplash.jpg"
+        image: "assets/keighla-exum-wnn9GuvXIZ4-unsplash.jpg",
+        quantity: 0
 
     }
 ]
@@ -109,6 +121,9 @@ const cart = [];
 
 const addToCart = (event) => {
     const grabInventory = inventory.find(food => food.id === event.target.id);
+    const existingCartItem = cart.find(food => food.id === event.target.id);
+    if (existingCartItem === undefined) {
+        grabInventory.quantity++;
     cart.push(grabInventory);
     const cartDiv = document.createElement('div');
     cartDiv.classList.add('cart-item');
@@ -116,7 +131,8 @@ const addToCart = (event) => {
     image.src = grabInventory.image;
     image.classList.add('item-image');
     const titleSpan = document.createElement('span');
-    titleSpan.innerText = grabInventory.name;
+    titleSpan.setAttribute('data-id', grabInventory.id)
+    titleSpan.innerText = `${grabInventory.name} x${grabInventory.quantity}`;
     const priceSpan = document.createElement('span');
     priceSpan.innerText = `$${grabInventory.price}`;
     const removeButton = document.createElement('button');
@@ -131,37 +147,56 @@ const addToCart = (event) => {
     const cartItem = document.querySelector('.cart-items');
     cartItem.append(cartDiv);
 
-
-    //functionally removes cart-item but doesn't update the total
-
     removeButton.addEventListener('click', (event) => {
         const cartThing = event.target.parentNode;
         cartThing.remove();
+        //cart.filter(item => )
         updateTotal()
     })
 
-    updateTotal()
+} else {
+    existingCartItem.quantity++;
+    const existingHTML = document.querySelector(`[data-id=${existingCartItem.id}]`);
+    existingHTML.innerText = `${existingCartItem.name} x${existingCartItem.quantity}`
+    console.log(existingHTML)
 }
+
+
+
+updateTotal()
+}
+
+function updateTotal() {
+    let total = 0;
+    cart.forEach((item) => (total += item.price * item.quantity));
+    document.querySelector('.cart-total-price').innerText = `Total: $${total}`;
+
+}
+
+
+
+
 const buttons = document.querySelectorAll('.add-to-cart-button');
 
 for (let button of buttons){
     button.addEventListener('click', addToCart)
 };
 
+//was trying go get it so hovering over shop item causes the description to appear
 
-//Have to make a function to make remove buttons active
+// const individualShopItem = document.querySelectorAll('.individual-shop-item');
+// individualShopItem.addEventListener('mouseover', () => {
+//     const itemDescription = getElementById('description');
+//     itemDescription.style.display = 'block'
+// });
 
 
 
 
 
 
-function updateTotal() {
-    let total = 0;
-    cart.forEach((item) => (total += item.price));
-    document.querySelector('.cart-total-price').innerText = `Total: $${total}`;
 
-}
+
 
 //to open checkout
 const checkoutPopUp = document.querySelector('.checkout-popup');
